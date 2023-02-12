722e8390e2da6f8813c161e003c329a873ff9285
Tunniedrey <115155502+Tunniedrey@users.noreply.github.com>
2023-02-12 23:56:07 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+const Rooms = [
+    {
+        'codeName': 'room1',
+        'roomType': 'Standard',
+        'price': 1000
+    },
+    {
+        'codeName': 'room2',
+        'roomType': 'Suite',
+        'price': 1500
+    },
+    {
+        'codeName': 'room3',
+        'roomType': 'Premium',
+        'price': 2000
+    },
+    {
+        'codeName': 'room4',
+        'roomType': 'Suite',
+        'price': 1500
+    },
+    {
+        'codeName': 'room5',
+        'roomType': 'Premium',
+        'price': 2000
+    },
+    {
+        'codeName': 'room6',
+        'roomType': 'Standard',
+        'price': 1000
+    },
+    {
+        'codeName': 'room7',
+        'roomType': 'Premium',
+        'price': 2000
+    },
+    {
+        'codeName': 'room8',
+        'roomType': 'Standard',
+        'price': 1000
+    },
+]
+
+module.exports = Rooms;
